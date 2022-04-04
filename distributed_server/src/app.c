84d0026935e51c1de5c6fb5cd956b0c918527dba
jps12 <jps12carvalho@gmail.com>
2022-04-04 20:57:10 -0300

    Configuring sockets

--- distributed_server/src/app.c
+++ distributed_server/src/app.c
@@ -5,14 +5,15 @@
 #include "fileio.h"
 #include "app.h"
 #include "gpio.h"
+#include "socket.h"
 
 #define FILE_SIZE 2000
+#define SOCKET_PORT 10140
 
 cJSON *request_json = NULL, *ip_servidor_central;
 
 void app_init(){
     app_config();
-    
 }
 
 void app_config(){
@@ -25,4 +26,5 @@ void app_read_config(){
     fileio_read_file("assets/config_terreo.json", buffer);
 
     request_json = cJSON_Parse(buffer);
+    socket_configure(SOCKET_PORT, cJSON_GetObjectItem(request_json, "ip_servidor_central")->valuestring);
 }
---
+++ distributed_server/src/socket.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+struct sockaddr_in local_address, server_address;
+int local_socket, opt = 1;
+
+void socket_configure(int socket_port, char *ip_servidor){
+    if ((local_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("socket failed");
+        return;
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons( socket_port );
+
+    if(inet_pton(AF_INET, ip_servidor, &server_address.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return;
+    }
+
+    if (connect(local_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return;
+    }
+}
